94f8f8b7d365c33b19eb6aac0d816f2d07354494
Yu-L <huangyeluwei@163.com>
2020-03-02 21:10:27 +0800

    first commit

---
+++ train_model/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+    File Name: __init__.py.py
+    Description:
+    
+Created by Yu Liu on 2019/8/20 2:45 PM.
+"""
+from .load_data import CNVDataGenerator, CNVDataGenerator2, CNVDataGenerator3
+from .train_run import train, train2
+from .evaluate_run import evaluate
+# from .cv_train import cv_train
+# from .evaluate_run import evaluate
+# from .multinodes_cvtrain import mnd_train
